b18d6e668400ca2bb79d8d3093e37b4087e5d711
othermully <ryancbmullins6@gmail.com>
2026-04-16 20:26:27 -0300

    version: i'm stuck again

--- json.hpp
+++ json.hpp
@@ -3,10 +3,14 @@
 #include <cstddef>
 #include <iostream>
 #include <map>
+#include <optional>
+#include <stdexcept>
 #include <string>
 #include <string_view>
 #include <vector>
 #include <variant>
+
+// TODO: Need to build an API to allow users to easily access JSON key/values
 
 namespace JSONParser{
 class Parser{
@@ -15,10 +19,6 @@ public:
   struct JSON;
   using Object = std::map<std::string, JSON>;
   using Array  = std::vector<JSON>;
-
-  JSON parseObject(const std::string& str, size_t& i);
-  JSON parseString(const std::string& str, size_t& i);
-  JSON parseArray(const std::string& str, size_t& i);
 
   struct JSON{
     std::variant<
@@ -30,18 +30,9 @@ public:
       > value;
   };
 
-  void skipWhitespace(const std::string& str, size_t& i){
-    while (i < str.size() && std::isspace(str[i])) {
-      ++i;
-    }
-  }
-
-  std::string_view getSubstring(std::string_view sv, size_t& i){
-    return sv.substr(i, sv.size());
-  }
-
   // Dispatcher
-  JSON parseValue(const std::string& str, size_t& i){
+  JSON parseValue(const std::string& str, size_t& i)
+  {
     skipWhitespace(str, i);
 
     if (str[i] == '"') {
@@ -60,7 +51,24 @@ public:
     return JSON{}; // This should be handled, return empty struct for now
   }
 
-  JSON parseNumber(const std::string& str, size_t& i){
+
+
+  
+private:
+  void skipWhitespace(const std::string& str, size_t& i)
+  {
+    while (i < str.size() && std::isspace(str[i])) {
+      ++i;
+    }
+  }
+
+  std::string_view getSubstring(std::string_view sv, size_t& i)
+  {
+    return sv.substr(i, sv.size());
+  }
+
+  JSON parseNumber(const std::string& str, size_t& i)
+  {
     skipWhitespace(str, i);
     double d{};
     std::string temp{};
@@ -73,9 +81,91 @@ public:
     return JSON{d};
   }
 
+  JSON parseObject(const std::string& str, size_t& i)
+  {
+    skipWhitespace(str, i);
+    Object obj{};
+
+    ++i; // Consume the opening '{' - Expect a key string
+    while (true) {
+      std::string key = std::get<std::string>(parseString(str, i).value);
+      if (str[i] != ':') {
+        throw std::runtime_error("Expected ':'");
+      }
+
+      ++i; // Consume the ':'
+      skipWhitespace(str, i);
+
+      // expect value here
+      JSON value = parseValue(str, i);
+      skipWhitespace(str, i);
+
+      obj[key] = value;
+
+      if (str[i] == ',') {
+        skipWhitespace(str, i);
+        ++i; // consume ','
+        continue;
+      }
+
+      if (str[i] == '}') {
+        ++i; // consume the closing '}'
+        return JSON{obj};
+      }
+      else {
+        throw std::runtime_error("Didn't receive correct char");
+      }
+    }
+  }
+
+  JSON parseArray(const std::string& str, size_t& i)
+  {
+    Array arr;
+    skipWhitespace(str, i);
+    ++i; // consume the opening '['
+
+    while (true) {
+      skipWhitespace(str, i);
+      JSON value = parseValue(str, i);
+      arr.push_back(value);
+
+      if (str[i] == ',') {
+        skipWhitespace(str, i);
+        ++i;
+        continue;
+      }
+
+      if (str[i] == ']') {
+        ++i; // consume closing ']'
+        return JSON{arr};
+      }
+    }
+  }
+
+  JSON parseString(const std::string& str, size_t& i)
+  {
+    skipWhitespace(str, i);
+    std::string s{};
+    ++i; // Consume the opening '"'
+    if (str[i] == '"') {
+      ++i; // consume closing '"'
+      return JSON{s};
+    }
+
+  }
+};
+
+class API{
+// TODO: Builder JSON key/value access functions here
+// TODO: I think this should also serialize parsed JSON in order to send back to client
+// Access JSON objects like this: JSON j = jparser.parseValue(string);
+// std::string name = j["name"];
+// find type -> get value -> access fields/array elements
+public:
+  std::optional<std::string> getValue(Parser::JSON& t, std::string_view input_key);
 
 private:
-
+  void getType();
 
 };
 
